8194be1e615f60526d50c95495707819a0ab61a5
Alfonso Dumlao <alfonsodumlao@Users-Air.home>
2019-01-08 17:32:47 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+
+
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardFour);
